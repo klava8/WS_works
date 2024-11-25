dfb4a33c09f461dae1fc9229fd9776d75bb6ee7d
StudentBI46 <StudentBI46@SYN13-PC-2211.alabuga.local>
2024-11-25 15:09:35 +0300

    created model posts

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import *
 
 # Create your models here.
+class Posts(models.Model):
+    image = TextField()
+    title = CharField(max_length=80)
+    description = TextField()
+    comments = TextField()
+    likes = IntegerField()
